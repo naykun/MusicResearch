376a3f85d7690fc559e179a986beee682364ee3b
Somedaywilldo <714439471@qq.com>
2018-07-23 01:21:02 +0800

    add a minimized runnable magenta and complete numpy to midi task

--- LSTM_Music_Generation/how_to_generate_numpy.py
+++ LSTM_Music_Generation/how_to_generate_numpy.py
@@ -1,11 +1,11 @@
 from sequence_example_lib import *
 import os
 import tensorflow as tf
-sequence_example_file = ('/media/kun/CEF282C9F282B4ED/TsinghuaResearch/Wikifonia_basic_rnn_sequence_examples/training_melodies.tfrecord')
+sequence_example_file = ('Wikifonia_basic_rnn_sequence_examples/eval_melodies.tfrecord')
 sequence_example_file_paths = tf.gfile.Glob(
     os.path.expanduser(sequence_example_file))
 
-inputs, labels, lengths = get_numpy_from_tf_sequence_example( input_size=74,
+inputs, labels, lengths = get_numpy_from_tf_sequence_example( input_size=38,
                                     sequence_example_file_paths = sequence_example_file_paths,
                                     shuffle = False)
 
